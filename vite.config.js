d7cae1f20f2811579cfa2e2c25211fcb9861e1a1
adam-ctrlc <a2e56b85@gmail.com>
2025-04-06 22:56:40 +0800

    something

--- vite.config.js
+++ vite.config.js
@@ -2,22 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import flowbiteReact from 'flowbite-react/plugin/vite';
-import { terser } from 'rollup-plugin-terser';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), flowbiteReact()],
   build: {
-    minify: 'esbuild', // Ensure minification using esbuild
-    terserOptions: {
-      compress: {
-        drop_console: true, // Removes console.* statements
-      },
-    },
-    rollupOptions: {
-      plugins: [
-        terser(), // Apply terser plugin for minification and drop console statements
-      ],
-    },
+    minify: 'esbuild',
+    rollupOptions: {},
   },
 });
